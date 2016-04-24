dfef7e9dac51e76a8f52e679f66ada5fd3fc574d
Ignacio Álvarez <ignacioalvarez92@gmail.com>
2016-04-24 19:56:11 -0300

    Wip

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,27 +2,25 @@ import { PropTypes } from 'React';
 import { Flexbox, FlexItem } from 'flexbox-react';
 import header from '~/css/components/header.css';
 
-function Layout({ children }) {
-  return (
-    <Flexbox flexDirection="column" alignItems="center" width="100%">
-      <Flexbox
-        className={header.container}
-        width="100%" height="350px"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="space-around"
-        padding="40px"
-      >
-        <span className={header.text}>flexbox-react</span>
-        <iframe
-          src="https://ghbtns.com/github-btn.html?user=nachoaIvarez&repo=flexbox-react&type=star&count=true&size=large"
-          frameBorder="0" scrolling="0" width="160px" height="30px"
-        ></iframe>
-      </Flexbox>
-      <FlexItem padding="40px" width="666px">{children}</FlexItem>
+const Layout = ({ children }) =>
+  (<Flexbox flexDirection="column" alignItems="center" width="100%">
+    <Flexbox
+      alignItems="center"
+      className={header.container}
+      flexDirection="column"
+      height="350px"
+      justifyContent="space-around"
+      padding="40px"
+      width="100%"
+    >
+      <span className={header.text}>flexbox-react</span>
+      <iframe
+        src="https://ghbtns.com/github-btn.html?user=nachoaIvarez&repo=flexbox-react&type=star&count=true&size=large"
+        frameBorder="0" scrolling="0" width="160px" height="30px"
+      ></iframe>
     </Flexbox>
-  );
-}
+    <FlexItem padding="40px" maxWidth="666px" >{children}</FlexItem>
+  </Flexbox>);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,

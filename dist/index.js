6ee83c8791c5d8a6279acfbdf31c1fce18f920e5
Matias Olivera <moliverafreire@gmail.com>
2016-06-22 19:23:46 -0300

    Move <FlexItem /> props to <Flexbox />

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.FlexItem = exports.Flexbox = undefined;
+exports.default = undefined;
 
 var _Flexbox = require('./Flexbox');
 
 var _Flexbox2 = _interopRequireDefault(_Flexbox);
 
-var _FlexItem = require('./FlexItem');
-
-var _FlexItem2 = _interopRequireDefault(_FlexItem);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.Flexbox = _Flexbox2.default;
-exports.FlexItem = _FlexItem2.default;
+exports.default = _Flexbox2.default;

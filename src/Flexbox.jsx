9e6364e344fed11a70c755a7f9668778f7642110
Ignacio Álvarez <ignacioalvarez92@gmail.com>
2017-04-19 20:43:21 -0300

    Update dependencies and run prettier over codebase

--- src/Flexbox.jsx
+++ src/Flexbox.jsx
@@ -35,34 +35,34 @@ const Flexbox = styled(({
   width,
   ...props
 }) => React.createElement(element, props, children))`
-  ${props => props.alignContent ? `align-content: ${props.alignContent};` : ''}
-  ${props => props.alignSelf ? `align-self: ${props.alignSelf};` : ''}
-  ${props => props.alignItems ? `align-items: ${props.alignItems};` : ''}
-  ${props => props.display ? `display: ${props.display};` : ''}
-  ${props => props.flex ? `flex: ${props.flex};` : ''}
-  ${props => props.flexBasis ? `flex-basis: ${props.flexBasis};` : ''}
-  ${props => props.flexDirection ? `flex-direction: ${props.flexDirection};` : ''}
-  ${props => props.flexGrow ? `flex-grow: ${props.flexGrow};` : ''}
-  ${props => props.flexShrink ? `flex-shrink: ${props.flexShrink};` : ''}
-  ${props => props.flexWrap ? `flex-wrap: ${props.flexWrap};` : ''}
-  ${props => props.height ? `height: ${props.height};` : ''}
-  ${props => props.justifyContent ? `justify-content: ${props.justifyContent};` : ''}
-  ${props => props.margin ? `margin: ${props.margin};` : ''}
-  ${props => props.marginBottom ? `margin-bottom: ${props.marginBottom};` : ''}
-  ${props => props.marginLeft ? `margin-left: ${props.marginLeft};` : ''}
-  ${props => props.marginRight ? `margin-right: ${props.marginRight};` : ''}
-  ${props => props.marginTop ? `margin-top: ${props.marginTop};` : ''}
-  ${props => props.maxHeight ? `max-height: ${props.maxHeight};` : ''}
-  ${props => props.maxWidth ? `max-width: ${props.maxWidth};` : ''}
-  ${props => props.minHeight ? `min-height: ${props.minHeight};` : ''}
-  ${props => props.minWidth ? `min-width: ${props.minWidth};` : ''}
-  ${props => props.order ? `order: ${props.order};` : ''}
-  ${props => props.padding ? `padding: ${props.padding};` : ''}
-  ${props => props.paddingBottom ? `padding-bottom: ${props.paddingBottom};` : ''}
-  ${props => props.paddingLeft ? `padding-left: ${props.paddingLeft};` : ''}
-  ${props => props.paddingRight ? `padding-right: ${props.paddingRight};` : ''}
-  ${props => props.paddingTop ? `padding-top: ${props.paddingTop};` : ''}
-  ${props => props.width ? `width: ${props.width};` : ''}
+  ${props => (props.alignContent ? `align-content: ${props.alignContent};` : '')}
+  ${props => (props.alignSelf ? `align-self: ${props.alignSelf};` : '')}
+  ${props => (props.alignItems ? `align-items: ${props.alignItems};` : '')}
+  ${props => (props.display ? `display: ${props.display};` : '')}
+  ${props => (props.flex ? `flex: ${props.flex};` : '')}
+  ${props => (props.flexBasis ? `flex-basis: ${props.flexBasis};` : '')}
+  ${props => (props.flexDirection ? `flex-direction: ${props.flexDirection};` : '')}
+  ${props => (props.flexGrow ? `flex-grow: ${props.flexGrow};` : '')}
+  ${props => (props.flexShrink ? `flex-shrink: ${props.flexShrink};` : '')}
+  ${props => (props.flexWrap ? `flex-wrap: ${props.flexWrap};` : '')}
+  ${props => (props.height ? `height: ${props.height};` : '')}
+  ${props => (props.justifyContent ? `justify-content: ${props.justifyContent};` : '')}
+  ${props => (props.margin ? `margin: ${props.margin};` : '')}
+  ${props => (props.marginBottom ? `margin-bottom: ${props.marginBottom};` : '')}
+  ${props => (props.marginLeft ? `margin-left: ${props.marginLeft};` : '')}
+  ${props => (props.marginRight ? `margin-right: ${props.marginRight};` : '')}
+  ${props => (props.marginTop ? `margin-top: ${props.marginTop};` : '')}
+  ${props => (props.maxHeight ? `max-height: ${props.maxHeight};` : '')}
+  ${props => (props.maxWidth ? `max-width: ${props.maxWidth};` : '')}
+  ${props => (props.minHeight ? `min-height: ${props.minHeight};` : '')}
+  ${props => (props.minWidth ? `min-width: ${props.minWidth};` : '')}
+  ${props => (props.order ? `order: ${props.order};` : '')}
+  ${props => (props.padding ? `padding: ${props.padding};` : '')}
+  ${props => (props.paddingBottom ? `padding-bottom: ${props.paddingBottom};` : '')}
+  ${props => (props.paddingLeft ? `padding-left: ${props.paddingLeft};` : '')}
+  ${props => (props.paddingRight ? `padding-right: ${props.paddingRight};` : '')}
+  ${props => (props.paddingTop ? `padding-top: ${props.paddingTop};` : '')}
+  ${props => (props.width ? `width: ${props.width};` : '')}
 `;
 
 Flexbox.propTypes = {
@@ -74,20 +74,8 @@ Flexbox.propTypes = {
     'space-between',
     'stretch',
   ]),
-  alignItems: PropTypes.oneOf([
-    'baseline',
-    'center',
-    'flex-end',
-    'flex-start',
-    'stretch',
-  ]),
-  alignSelf: PropTypes.oneOf([
-    'baseline',
-    'center',
-    'flex-end',
-    'flex-start',
-    'stretch',
-  ]),
+  alignItems: PropTypes.oneOf(['baseline', 'center', 'flex-end', 'flex-start', 'stretch']),
+  alignSelf: PropTypes.oneOf(['baseline', 'center', 'flex-end', 'flex-start', 'stretch']),
   children: PropTypes.node,
   display: PropTypes.oneOf(['flex', 'inline-flex']),
   element: PropTypes.oneOf([
@@ -103,12 +91,7 @@ Flexbox.propTypes = {
   ]),
   flex: PropTypes.string,
   flexBasis: PropTypes.string,
-  flexDirection: PropTypes.oneOf([
-    'column-reverse',
-    'column',
-    'row-reverse',
-    'row',
-  ]),
+  flexDirection: PropTypes.oneOf(['column-reverse', 'column', 'row-reverse', 'row']),
   flexGrow: PropTypes.number,
   flexShrink: PropTypes.number,
   flexWrap: PropTypes.oneOf(['nowrap', 'wrap-reverse', 'wrap']),
